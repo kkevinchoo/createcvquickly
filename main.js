8303bfae98e9a34df8680742eab0a4f3ca2fc5b2
kkevinchoo <kkevinchoo@gmail.com>
2024-06-25 15:52:46 +0000

    Can change color sidebar

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector("#color-one").addEventListener(
+        "click",
+        function() {
+            document.querySelector(".sidebar").classList.remove("sidebar-two", "sidebar-three")
+            document.querySelector(".sidebar").classList.add("sidebar-one")
+        }
+    )
+    document.querySelector("#color-two").addEventListener(
+        "click",
+        function() {
+            document.querySelector(".sidebar").classList.remove("sidebar-one", "sidebar-three")
+            document.querySelector(".sidebar").classList.add("sidebar-two")
+        }
+    )
+    document.querySelector("#color-three").addEventListener(
+        "click",
+        function() {
+            document.querySelector(".sidebar").classList.remove("sidebar-one", "sidebar-two")
+            document.querySelector(".sidebar").classList.add("sidebar-three")
+        })
+});
+
+
 document.getElementById('upload-photo').addEventListener('change', function() {
     const reader = new FileReader();
     reader.onload = function(e) {
